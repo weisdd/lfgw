0710f435e098bf0f0abce1ffa36396819e0a527d
Igor Beliakov <demtis.register@gmail.com>
2022-04-23 17:21:05 +0200

    Expand CLI test to all booleans

--- internal/gw/main_test.go
+++ internal/gw/main_test.go
@@ -11,74 +11,132 @@ import (
 )
 
 func Test_newApplication(t *testing.T) {
-	// TODO: test url error
-	// TODO: test boolean independently to make sure they don't override each other
-	debug := true
-	logFormat := "json"
-	logNoColor := true
-	logRequests := true
-	upstreamURL := "http://localhost"
-	optimizeExpression := true
-	enableDeduplication := true
-	safeMode := true
-	setProxyHeaders := true
-	setGomaxProcs := true
-	aclPath := "ACL.yaml"
-	assumedRoles := true
-	oidcRealmURL := "http://localhost2"
-	oidcClientID := "grafana"
-	port := 9999
-	readTimeout := 6 * time.Second
-	writeTimeout := 7 * time.Second
-	gracefulShutdownTimeout := 8 * time.Second
-
-	set := flag.NewFlagSet("test", 0)
-	set.Bool("debug", debug, "doc")
-	set.String("log-format", logFormat, "doc")
-	set.Bool("log-no-color", logNoColor, "doc")
-	set.Bool("log-requests", logRequests, "doc")
-	set.String("upstream-url", upstreamURL, "doc")
-	set.Bool("optimize-expressions", optimizeExpression, "doc")
-	set.Bool("enable-deduplication", enableDeduplication, "doc")
-	set.Bool("safe-mode", safeMode, "doc")
-	set.Bool("set-proxy-headers", setProxyHeaders, "doc")
-	set.Bool("set-gomax-procs", setGomaxProcs, "doc")
-	set.String("acl-path", aclPath, "doc")
-	set.Bool("assumed-roles", assumedRoles, "doc")
-	set.String("oidc-realm-url", oidcRealmURL, "doc")
-	set.String("oidc-client-id", oidcClientID, "doc")
-	set.Int("port", port, "doc")
-	set.Duration("read-timeout", readTimeout, "doc")
-	set.Duration("write-timeout", writeTimeout, "doc")
-	set.Duration("graceful-shutdown-timeout", gracefulShutdownTimeout, "doc")
-	c := cli.NewContext(nil, set, nil)
-
-	appUpstreamURL, err := url.Parse(upstreamURL)
-	assert.Nil(t, err)
-
-	want := application{
-		Debug:                   debug,
-		LogFormat:               logFormat,
-		LogNoColor:              logNoColor,
-		LogRequests:             logRequests,
-		UpstreamURL:             appUpstreamURL,
-		OptimizeExpressions:     optimizeExpression,
-		EnableDeduplication:     enableDeduplication,
-		SafeMode:                safeMode,
-		SetProxyHeaders:         setProxyHeaders,
-		SetGomaxProcs:           setGomaxProcs,
-		ACLPath:                 aclPath,
-		AssumedRolesEnabled:     assumedRoles,
-		OIDCRealmURL:            oidcRealmURL,
-		OIDCClientID:            oidcClientID,
-		Port:                    port,
-		ReadTimeout:             readTimeout,
-		WriteTimeout:            writeTimeout,
-		GracefulShutdownTimeout: gracefulShutdownTimeout,
+	// All these tests make sure only one boolean gets changed to true. Otherwise, there's always a risk that value for one field overrides another one.
+	tests := []struct {
+		name string
+		want application
+	}{
+		{
+			name: "debug",
+			want: application{Debug: true},
+		},
+		{
+			name: "log-no-color",
+			want: application{LogNoColor: true},
+		},
+		{
+			name: "log-requests",
+			want: application{LogRequests: true},
+		},
+		{
+			name: "optimize-expressions",
+			want: application{OptimizeExpressions: true},
+		},
+		{
+			name: "enable-deduplication",
+			want: application{EnableDeduplication: true},
+		},
+		{
+			name: "safe-mode",
+			want: application{SafeMode: true},
+		},
+		{
+			name: "set-proxy-headers",
+			want: application{SetProxyHeaders: true},
+		},
+		{
+			name: "set-gomax-procs",
+			want: application{SetGomaxProcs: true},
+		},
+		{
+			name: "assumed-roles",
+			want: application{AssumedRolesEnabled: true},
+		},
 	}
 
-	got, err := newApplication(c)
-	assert.Nil(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			set := flag.NewFlagSet("test", 0)
+			set.Bool(tt.name, true, "doc")
+			c := cli.NewContext(nil, set, nil)
 
-	assert.Equal(t, want, got)
+			// Needed since Parse is called in the function
+			tt.want.UpstreamURL = &url.URL{}
+
+			got, err := newApplication(c)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+
+	t.Run("Full application struct", func(t *testing.T) {
+		debug := true
+		logFormat := "json"
+		logNoColor := true
+		logRequests := true
+		upstreamURL := "http://localhost"
+		optimizeExpression := true
+		enableDeduplication := true
+		safeMode := true
+		setProxyHeaders := true
+		setGomaxProcs := true
+		aclPath := "ACL.yaml"
+		assumedRoles := true
+		oidcRealmURL := "http://localhost2"
+		oidcClientID := "grafana"
+		port := 9999
+		readTimeout := 6 * time.Second
+		writeTimeout := 7 * time.Second
+		gracefulShutdownTimeout := 8 * time.Second
+
+		set := flag.NewFlagSet("test", 0)
+		set.Bool("debug", debug, "doc")
+		set.String("log-format", logFormat, "doc")
+		set.Bool("log-no-color", logNoColor, "doc")
+		set.Bool("log-requests", logRequests, "doc")
+		set.String("upstream-url", upstreamURL, "doc")
+		set.Bool("optimize-expressions", optimizeExpression, "doc")
+		set.Bool("enable-deduplication", enableDeduplication, "doc")
+		set.Bool("safe-mode", safeMode, "doc")
+		set.Bool("set-proxy-headers", setProxyHeaders, "doc")
+		set.Bool("set-gomax-procs", setGomaxProcs, "doc")
+		set.String("acl-path", aclPath, "doc")
+		set.Bool("assumed-roles", assumedRoles, "doc")
+		set.String("oidc-realm-url", oidcRealmURL, "doc")
+		set.String("oidc-client-id", oidcClientID, "doc")
+		set.Int("port", port, "doc")
+		set.Duration("read-timeout", readTimeout, "doc")
+		set.Duration("write-timeout", writeTimeout, "doc")
+		set.Duration("graceful-shutdown-timeout", gracefulShutdownTimeout, "doc")
+		c := cli.NewContext(nil, set, nil)
+
+		appUpstreamURL, err := url.Parse(upstreamURL)
+		assert.Nil(t, err)
+
+		want := application{
+			Debug:                   debug,
+			LogFormat:               logFormat,
+			LogNoColor:              logNoColor,
+			LogRequests:             logRequests,
+			UpstreamURL:             appUpstreamURL,
+			OptimizeExpressions:     optimizeExpression,
+			EnableDeduplication:     enableDeduplication,
+			SafeMode:                safeMode,
+			SetProxyHeaders:         setProxyHeaders,
+			SetGomaxProcs:           setGomaxProcs,
+			ACLPath:                 aclPath,
+			AssumedRolesEnabled:     assumedRoles,
+			OIDCRealmURL:            oidcRealmURL,
+			OIDCClientID:            oidcClientID,
+			Port:                    port,
+			ReadTimeout:             readTimeout,
+			WriteTimeout:            writeTimeout,
+			GracefulShutdownTimeout: gracefulShutdownTimeout,
+		}
+
+		got, err := newApplication(c)
+		assert.Nil(t, err)
+
+		assert.Equal(t, want, got)
+	})
 }
